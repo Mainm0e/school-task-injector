56322800890613e258f7f19b18b8039b3c2459b1
TheGitGuru <mainm0e1996@gmail.com>
2024-03-19 16:54:47 +0200

    update some stuff

--- injector.go
+++ injector.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -22,8 +21,6 @@ func mergePrograms(targetBinaryPath, injectorBinaryPath string) error {
 	defer targetFile.Close()
 
 	// Write the injector binary to the end of the target binary
-	fmt.Println("checking \n", injectorBinary)
-	fmt.Println("checking \n", targetFile)
 	_, err = targetFile.Write(injectorBinary)
 	if err != nil {
 		return err
@@ -46,64 +43,3 @@ func main() {
 		log.Fatal(err)
 	}
 }
-
-/*
-package main
-
-import (
-	"io/ioutil"
-	"log"
-	"os"
-)
-
-func mergePrograms(targetBinaryPath, injectorBinaryPath string) error {
-	// Read the injector binary
-	injectorBinary, err := ioutil.ReadFile(injectorBinaryPath)
-	if err != nil {
-		return err
-	}
-
-	// Open the target binary in read mode
-	targetFile, err := os.OpenFile(targetBinaryPath, os.O_RDWR, 0644)
-	if err != nil {
-		return err
-	}
-	defer targetFile.Close()
-
-	// Get the size of the target binary
-	targetStat, err := targetFile.Stat()
-	if err != nil {
-		return err
-	}
-	targetSize := targetStat.Size()
-
-	// Seek to the end of the target binary
-	_, err = targetFile.Seek(targetSize, 0)
-	if err != nil {
-		return err
-	}
-
-	// Write the injector binary to the end of the target binary
-	_, err = targetFile.Write(injectorBinary)
-	if err != nil {
-		return err
-	}
-
-	log.Printf("Injected %s into %s\n", injectorBinaryPath, targetBinaryPath)
-	return nil
-}
-
-func main() {
-	if len(os.Args) != 3 {
-		log.Fatalf("Usage: %s <target> <injector>", os.Args[0])
-	}
-
-	target := os.Args[1]
-	injector := os.Args[2]
-
-	err := mergePrograms(target, injector)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-*/
